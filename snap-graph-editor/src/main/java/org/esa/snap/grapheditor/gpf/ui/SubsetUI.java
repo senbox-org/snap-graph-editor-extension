486b909699cbc99be615f70c3d4c0badb34b1ecf
Martino Ferrari <manda.mgf@gmail.com>
2020-12-09 15:57:49 +0100

    updated import to locationtech package

--- snap-graph-editor/src/main/java/org/esa/snap/grapheditor/gpf/ui/SubsetUI.java
+++ snap-graph-editor/src/main/java/org/esa/snap/grapheditor/gpf/ui/SubsetUI.java
@@ -15,11 +15,11 @@
  */
 package org.esa.snap.grapheditor.gpf.ui;
 
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.LinearRing;
-import com.vividsolutions.jts.io.WKTReader;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.LinearRing;
+import org.locationtech.jts.io.WKTReader;
 import org.esa.snap.core.datamodel.GeoPos;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.grapheditor.gpf.ui.worldmap.NestWorldMapPane;
